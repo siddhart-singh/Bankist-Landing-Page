34129ea50114f611dfbfeac1f7930f4bd55c3467
sidSing <99249070+sidSing@users.noreply.github.com>
2022-06-30 05:16:02 -0700

    Update script.js

--- script.js
+++ script.js
@@ -13,27 +13,9 @@ const nav = document.querySelector('.nav');
 const section1 = document.querySelector('#section--1');
 document.getElementById('section--1');
 
-const openModal = function (e) {
-  e.preventDefault();
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-};
 
-const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
 
-btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
-
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
-  }
-});
 
 //Page Navigation
 // document.querySelectorAll('.nav__link').forEach(function (el) {
